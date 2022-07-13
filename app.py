b8850b16bde037823f251f5dae060937350d27a9
Aidan Carter <aidan.kyle.carter@gmail.com>
2022-07-13 14:46:53 -0700

    reimb_dao Read set to filter

--- app.py
+++ app.py
@@ -6,10 +6,23 @@ import datetime
 #(self, amount, submitted, type, descrip, receipt, author)
 rd = ReimbDao()
 now = datetime.datetime.now()
-re = Reimbursement(12.50, now, "travel", "tololol", "idgaf", 2)
+re = Reimbursement(12.50, now, "other", "tololol", "idgaf", 4)
 
 # print(rd.create_reimb(re))
-res = rd.get_reimb_by_user(4)
+
+
+# r7 = rd.get_reimb(7)
+# r4 = rd.get_reimb(4)
+#
+# r7.set_status('approved')
+# r4.set_status('denied')
+# r7.set_resolver(1)
+# r4.set_resolver(1)
+# rd.update_reimb(r7)
+# rd.update_reimb(r4)
+
+res = rd.get_reimbs(3, None, None)
 for reimb in res:
     print(reimb.to_dict())
+
 
--- dao/reimb_dao.py
+++ dao/reimb_dao.py
@@ -1,5 +1,6 @@
 from model.reimbursement import Reimbursement
 import psycopg
+import datetime
 
 
 class ReimbDao:
@@ -20,12 +21,39 @@ class ReimbDao:
         return "this blows"
 
 # Read
-    def get_reimb_by_user(self, user_id):
-        reimbs = []
+    def get_reimb(self, reimb_id):
         with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
                              password="pass") as conn:
             with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM ers_reimbursement WHERE reimb_author = '{user_id}';")
+                cur.execute(f"SELECT * FROM ers_reimbursement WHERE reimb_id = '{reimb_id}';")
+                for line in cur:
+                    reimb = Reimbursement(line[1], line[2], line[5], line[6], line[7], line[8])
+                    reimb.set_id(line[0])
+                    reimb.set_status(line[4])
+                    reimb.set_resolved(line[3])
+                    reimb.set_resolver(line[9])
+                    return reimb
+
+    def get_reimbs(self, user_id, filter_status, filter_type):
+        reimbs = []
+        call = "SELECT * FROM ers_reimbursement"
+        if not user_id is None or not filter_status is None or not filter_type is None:
+            call = call + f" WHERE "
+            if not user_id is None:
+                call = call + f"reimb_author = '{user_id}'"
+                if not filter_status is None or not filter_type is None:
+                    call = call + " AND "
+            if not filter_status is None:
+                call = call + f"status = '{filter_status}'"
+                if not filter_type is None:
+                    call = call + " AND "
+            if not filter_type is None:
+                call = call + f"reimb_type = '{filter_type}'"
+            call = call + ";"
+        with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
+                             password="pass") as conn:
+            with conn.cursor() as cur:
+                cur.execute(call)
                 for line in cur:
                     reimb = Reimbursement(line[1], line[2], line[5], line[6], line[7], line[8])
                     reimb.set_id(line[0])
@@ -35,24 +63,18 @@ class ReimbDao:
                     reimbs.append(reimb)
             return reimbs
 
-    def get_all_reimbs(self):
-        reimbs = []
-        with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
-                             password="pass") as conn:
-            with conn.cursor() as cur:
-                cur.execute(f"SELECT * FROM ers_reimbursement;")
-                for line in cur:
-                    reimb = Reimbursement(line[1], line[2], line[5], line[6], line[7], line[8])
-                    reimb.set_id(line[0])
-                    reimb.set_status(line[4])
-                    reimb.set_resolved(line[3])
-                    reimb.set_resolver(line[9])
-                    reimbs.append(reimb)
-            return reimbs
-
-
-
+  
 # Update
+    def update_reimb(self, reimb_obj):
+        with psycopg.connect(host="localhost", port="5432", dbname="postgres", user="postgres",
+                             password="pass") as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"UPDATE ers_reimbursement SET status = '{reimb_obj.status}', resolved = "
+                            f"'{datetime.datetime.now()}', reimb_resolver = '{reimb_obj.resolver}' WHERE reimb_id = "
+                            f"'{reimb_obj.reimb_id}';")
+                conn.commit()
+                return f"Reimbursement request {reimb_obj.reimb_id} has been {reimb_obj.status}."
+
 
 
 # Delete
